67e1968576962e55186e19c741c1463273019026
cainaGomesDS <caina.silva@sptech.school>
2024-10-31 20:55:34 -0300

    Adicionando atualizações na tela de cadastro e nas suas funcionalidades.

--- telaCadastro/functions.js
+++ telaCadastro/functions.js
@@ -1,23 +1,52 @@
-function validarEmail() {
-    var email = document.getElementById('input_userEmail').value;
-    var emailValido = /^[^\s@]+@[^\s@]+\.(com|br|school)$/.test(email);
+function nomeSelecionado() {
+    var nome = document.getElementById('input_userNome').value
+    var tamanhoNome = nome.length
+    document.getElementById('input_userNome').style.backgroundColor = '#F9F9F9'
 
+    if (tamanhoNome < 4) {
+        document.getElementById('nomeRequisitos').style.display = 'block'
+        document.getElementById('nomeRequisitos').style.color = 'red'
+        document.getElementById('input_userNome').style.borderColor = 'red'
 
-    if (email) {
-        document.getElementById('input_userEmail').style.backgroundColor = "#f9f9f9"
-    } else {
+    } else if (tamanhoNome >= 4) {
+        document.getElementById('nomeRequisitos').style.display = 'none'
+        document.getElementById('input_userNome').style.borderColor = 'green'
 
     }
 
-
-    if (emailValido) {
-        document.getElementById('input_userEmail').style.borderColor = "green"
-    } else {
-        document.getElementById('input_userEmail').style.borderColor = "red"
-    }
 }
 
-function validarSenha(){
+
+function validarEmail() {
+    var email = document.getElementById('input_userEmail').value;
+    var emailRegra = /^[^\s@]+@[^\s@]+\.(com|br|school)$/.test(email);
+
+
+    document.getElementById('input_userEmail').style.backgroundColor = '#F9F9F9'
+
+
+
+    if (emailRegra) {
+        document.getElementById('input_userEmail').style.borderColor = 'green'
+    } else {
+        document.getElementById('input_userEmail').style.borderColor = 'red'
+    }
+
+    document.getElementById('emailRegra').style.color = emailRegra ? 'green' : 'red';
+}
+
+
+function mostrarEmailRegra() {
+    document.getElementById('emailRequisitos').style.display = 'block';
+}
+
+
+function ocultarEmailRegra() {
+    document.getElementById('emailRequisitos').style.display = 'none';
+}
+
+
+function validarSenha() {
     var senha = document.getElementById('input_userSenha').value;
 
     var senhaMinuscula = /[a-z]/.test(senha); // Verifica se tem pelo menos 
@@ -27,46 +56,46 @@ function validarSenha(){
     var senhaNumero = /[\d]/.test(senha); // Verifica se tem números de 0 a 9 na senha
     var senhaEspecial = /[\W_]/.test(senha); // Verifica se tem caracteres especiais
 
+    document.getElementById('input_userSenha').style.backgroundColor = '#F9F9F9'
 
     // Exibir os requisitos em vermelho ou verde conforme o usuário digita
-    document.getElementById("senhaMinuscula").style.color = senhaMinuscula ? "green" : "red";
-    document.getElementById("senhaMaiuscula").style.color = senhaMaiuscula ? "green" : "red";
-    document.getElementById("senhaNumero").style.color = senhaNumero ? "green" : "red";
-    document.getElementById("senhaEspecial").style.color = senhaEspecial ? "green" : "red";
-
-
-    if(senha){
-        document.getElementById('input_userSenha').style.backgroundColor = '#f9f9f9'
-    }
+    document.getElementById('senhaMinuscula').style.color = senhaMinuscula ? 'green' : 'red';
+    document.getElementById('senhaMaiuscula').style.color = senhaMaiuscula ? 'green' : 'red';
+    document.getElementById('senhaNumero').style.color = senhaNumero ? 'green' : 'red';
+    document.getElementById('senhaEspecial').style.color = senhaEspecial ? 'green' : 'red';
 
 
     if (senhaMinuscula && senhaMaiuscula && senhaNumero && senhaEspecial) {
-        document.getElementById('input_userSenha').style.borderColor = "green";
+        document.getElementById('input_userSenha').style.borderColor = 'green';
     } else {
-        document.getElementById('input_userSenha').style.borderColor = "red";
+        document.getElementById('input_userSenha').style.borderColor = 'red';
     }
 
 }
 
 
-function mostrarRequisitosSenha(){
-    document.getElementById("senhaRequisitos").style.display = "block";
+function mostrarRequisitosSenha() {
+    document.getElementById('senhaRequisitos').style.display = 'block';
 }
+
 
 function ocultarRequisitosSenha() {
-    document.getElementById("senhaRequisitos").style.display = "none";
+    document.getElementById('senhaRequisitos').style.display = 'none';
 }
 
-function validarSenhasIguais(){
+
+function validarSenhasIguais() {
     var senha = document.getElementById('input_userSenha').value
     var validacao = document.getElementById('input_validarSenha').value
 
-    if(senha == validacao){
+    document.getElementById('input_validarSenha').style.backgroundColor = '#F9F9F9'
+    if (senha == validacao) {
         document.getElementById('input_validarSenha').style.borderColor = 'green'
-    }else{
+    } else {
         document.getElementById('input_validarSenha').style.borderColor = 'red'
     }
 }
+
 
 function validarCadastro() {
     validarEmail();
@@ -74,15 +103,15 @@ function validarCadastro() {
     validarSenhasIguais();
 
     // Verifica se todos os campos estão verdes (válidos)
-    var camposValidos = document.getElementById('input_userName').value !== ""
-                        document.getElementById('input_userEmail').style.borderColor === "green" &&
-                        document.getElementById('input_userSenha').style.borderColor === "green" &&
-                        document.getElementById('input_validarSenha').style.borderColor === "green";
+    var camposValidos = document.getElementById('input_userNome').style.borderColor === 'green' &&
+        document.getElementById('input_userEmail').style.borderColor === 'green' &&
+        document.getElementById('input_userSenha').style.borderColor === 'green' &&
+        document.getElementById('input_validarSenha').style.borderColor === 'green';
 
     if (camposValidos) {
         // Redireciona para a página de login
-        window.location.href = "./lista11_ex_02.html";
+        window.location.href = './lista11_ex_02.html';
     } else {
-        alert("Por favor, corrija os erros ou preencha todos os campos e tente novamente.");
+        alert('Por favor, corrija os erros ou preencha todos os campos e tente novamente.');
     }
 }
