0fb176760af382f0b2e4b24a50827c3427f94cd8
cainaGomesDS <caina.silva@sptech.school>
2024-11-03 04:25:07 -0300

    Adicionando a tela index do projeto com algumas funções já operantes, como mudanças de tela, hovers e etc. Modifiquei os arquivos para estarem dentro da pasta da API web-data-viz para uso nas semanas seguintes.

---
+++ siteOficial/public/js/scriptLogin.js
@@ -0,0 +1,11 @@
+function validarLogin(){
+    var mensagem = ''
+    var nome = input_userNome.value;
+    var senha = input_userSenha.value;
+
+    if(nome == '' || senha == ''){
+        alert(`Por favor, corrija os erros ou preencha todos os campos e tente novamente.`)
+    }else{
+
+    }
+}
